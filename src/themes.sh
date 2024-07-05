9b4b7e9a5ac2321178a825c940fdea2bace1c4cd
Dysthesis <antheoraviel@protonmail.com>
2024-07-05 11:34:31 +1000

    colours: adjust to oledppuccin

--- src/themes.sh
+++ src/themes.sh
@@ -3,76 +3,28 @@
 SELECTED_THEME="$(tmux show-option -gv @tokyo-night-tmux_theme)"
 
 case $SELECTED_THEME in
-"storm")
-  declare -A THEME=(
-    ["background"]="#24283b"
-    ["foreground"]="#a9b1d6"
-    ["black"]="#414868"
-    ["blue"]="#7aa2f7"
-    ["cyan"]="#7dcfff"
-    ["green"]="#73daca"
-    ["magenta"]="#bb9af7"
-    ["red"]="#f7768e"
-    ["white"]="#a9b1d6"
-    ["yellow"]="#e0af68"
-
-    ["bblack"]="#414868"
-    ["bblue"]="#7aa2f7"
-    ["bcyan"]="#7dcfff"
-    ["bgreen"]="#41a6b5"
-    ["bmagenta"]="#bb9af7"
-    ["bred"]="#f7768e"
-    ["bwhite"]="#787c99"
-    ["byellow"]="#e0af68"
-  )
-  ;;
-
-"day")
-  declare -A THEME=(
-    ["background"]="#d5d6db"
-    ["foreground"]="#343b58"
-    ["black"]="#0f0f14"
-    ["blue"]="#34548a"
-    ["cyan"]="#0f4b6e"
-    ["green"]="#33635c"
-    ["magenta"]="#5a4a78"
-    ["red"]="#8c4351"
-    ["white"]="#343b58"
-    ["yellow"]="#8f5e15"
-
-    ["bblack"]="#9699a3"
-    ["bblue"]="#34548a"
-    ["bcyan"]="#0f4b6e"
-    ["bgreen"]="#33635c"
-    ["bmagenta"]="#5a4a78"
-    ["bred"]="#8c4351"
-    ["bwhite"]="#343b58"
-    ["byellow"]="#8f5815"
-  )
-  ;;
-
 *)
   # Default to night theme
   declare -A THEME=(
-    ["background"]="#1A1B26"
-    ["foreground"]="#a9b1d6"
-    ["black"]="#414868"
-    ["blue"]="#7aa2f7"
-    ["cyan"]="#7dcfff"
-    ["green"]="#73daca"
-    ["magenta"]="#bb9af7"
-    ["red"]="#f7768e"
-    ["white"]="#c0caf5"
-    ["yellow"]="#e0af68"
+    ["background"]="#000000"
+    ["foreground"]="#ffffff"
+    ["black"]="#11111b"
+    ["blue"]="#89b4fa"
+    ["cyan"]="#94e2d5"
+    ["green"]="#a6e3a1"
+    ["magenta"]="#cba6f7"
+    ["red"]="#f38ba8"
+    ["white"]="#cdd6f4"
+    ["yellow"]="#fab387"
 
-    ["bblack"]="#2A2F41"
-    ["bblue"]="#7aa2f7"
-    ["bcyan"]="#7dcfff"
-    ["bgreen"]="#41a6b5"
-    ["bmagenta"]="#bb9af7"
-    ["bred"]="#ff9e64"
-    ["bwhite"]="#787c99"
-    ["byellow"]="#e0af68"
+    ["bblack"]="#181825"
+    ["bblue"]="#74c7ec"
+    ["bcyan"]="#94e2d5"
+    ["bgreen"]="#a6e3a1"
+    ["bmagenta"]="#cba6f7"
+    ["bred"]="#f5c2e7"
+    ["bwhite"]="#ffffff"
+    ["byellow"]="#f9e2af"
   )
   ;;
 esac
